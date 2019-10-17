11f49bdb7294400abf5ade031004b02b998883f1
Cesar Mendoza <mendoza.cesar92@gmail.com>
2019-10-17 21:59:49 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
-  
-array = [1, 2, 3]
-array.each do |number|
-   puts number * number
+  new_array = []
+  array.each{|a| new_array.push(a*a)}
+  return new_array
 end
-end
+
+arr = [1, 2, 3, 4, 5]
+square_array(arr)
